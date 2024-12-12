1cae68e6cc97b94056a8da2cf2b61495ada6dd7b
MrJackSpade <loffredo.ap@gmail.com>
2024-12-11 21:26:51 -0700

    Update install_wine.sh

--- install_wine.sh
+++ install_wine.sh
@@ -263,7 +263,7 @@ pkill -f vncviewer
 
 Xvfb :1 -screen 0 1024x768x24 &
 sleep 3
-x11vnc -display :1 -forever -noshm -listen 0.0.0.0 -scale 0.5 &
+x11vnc -display :1 -forever -noshm -listen 0.0.0.0 -scale 0.625 &
 sleep 10
 export DISPLAY=:0
 export STARTUP="vncviewer 0.0.0.0:0 -fullscreen"
